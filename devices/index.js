bfb1daa3ff21442737097ddb96e9866528489dcf
Pierre Frisch <pierre_frisch@apple.com>
2018-05-21 16:24:21 -0700

    add support for SR-2108B-M24-5

--- devices/index.js
+++ devices/index.js
@@ -2,4 +2,5 @@ module.exports = {
   'eurolite-led-bar': require('./eurolite-led-bar'),
   'showtec-multidim2': require('./showtec-multidim2'),
   'stairville-led-par-56': require('./stairville-led-par-56'),
+  'ultra-pro-24ch-rdm': require('./ultra-pro-24ch-rdm'),
 }
