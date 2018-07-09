1cd46932bdeb97de87516cbc8f53ce591a8d073c
Max Stockner <mail@bluemaex.de>
2018-07-09 22:37:00 +0200

    update coding style to the project

--- index.js
+++ index.js
@@ -44,36 +44,36 @@ DmxPlatform.prototype.getDmxUniverses = function() {
 
 DmxPlatform.prototype.handleDmxDevice = function(universe, device) {
   switch(device.type) {
-  case 'ultra-pro-6rgbch-rdm':  {
+    case 'ultra-pro-6rgbch-rdm':  {
       return device.config.channelgroups.forEach((channel, num) => {
-          const subDevice = Object.assign({}, device, {
-              name: device.name + ':' + channel,
-              channels: [channel],
-              subNum: num
-          })
+        const subDevice = Object.assign({}, device, {
+          name: device.name + ':' + channel,
+          channels: [channel],
+          subNum: num
+        })
 
-          this.findAccessory(subDevice.name)
-              ? null
-              : this.createAccessory(universe, subDevice)
+        this.findAccessory(subDevice.name)
+            ? null
+            : this.createAccessory(universe, subDevice)
       })
-  }
+    }
 
-  case 'ultra-pro-24ch-rdm':
-  case 'showtec-multidim2': {
+    case 'ultra-pro-24ch-rdm':
+    case 'showtec-multidim2': {
       return device.config.channels.forEach((channel, num) => {
-          const subDevice = Object.assign({}, device, {
-              name: device.name + ':' + channel,
-              channels: [channel],
-              subNum: num
-          })
+        const subDevice = Object.assign({}, device, {
+            name: device.name + ':' + channel,
+            channels: [channel],
+            subNum: num
+        })
 
-          this.findAccessory(subDevice.name)
-          ? null
-          : this.createAccessory(universe, subDevice)
-  })
-  }
+        this.findAccessory(subDevice.name)
+            ? null
+            : this.createAccessory(universe, subDevice)
+      })
+    }
 
-  default: {
+    default: {
       return this.findAccessory(device.name)
         ? null
         : this.createAccessory(universe, device)
